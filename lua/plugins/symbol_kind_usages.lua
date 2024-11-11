a606c5a1b15e59ea13a8030a68cf5380e22ebde9
sami <sami@appscode.com>
2024-11-11 17:48:10 +0600

    Toggle symbol usage inlay hint
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/symbol_kind_usages.lua
+++ lua/plugins/symbol_kind_usages.lua
@@ -1,27 +1,30 @@
 return {
-	{
-		"Wansmer/symbol-usage.nvim",
-		event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-		config = function()
-			local SymbolKind = vim.lsp.protocol.SymbolKind
-			require("symbol-usage").setup({
-				kinds = {
-					SymbolKind.Function,
-					SymbolKind.Method,
-					SymbolKind.Interface,
-					SymbolKind.Class,
-					SymbolKind.Struct,
-					SymbolKind.Enum,
-					SymbolKind.Constant,
-					-- SymbolKind.Field,
-					-- SymbolKind.Variable,
-					-- SymbolKind.Object,
-				},
-			})
-		end,
-	},
+
 	{
 		"VidocqH/lsp-lens.nvim",
+		dependencies = {
+			{
+				"Wansmer/symbol-usage.nvim",
+				event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+				config = function()
+					local SymbolKind = vim.lsp.protocol.SymbolKind
+					require("symbol-usage").setup({
+						kinds = {
+							SymbolKind.Function,
+							SymbolKind.Method,
+							SymbolKind.Interface,
+							SymbolKind.Class,
+							SymbolKind.Struct,
+							SymbolKind.Enum,
+							SymbolKind.Constant,
+							-- SymbolKind.Field,
+							-- SymbolKind.Variable,
+							-- SymbolKind.Object,
+						},
+					})
+				end,
+			},
+		},
 		config = function()
 			local SymbolKind = vim.lsp.protocol.SymbolKind
 			require("lsp-lens").setup({
@@ -49,6 +52,29 @@ return {
 					SymbolKind.Struct,
 				},
 			})
+
+			local showing_usages = true
+			local toggle_usages = function()
+				showing_usages = not showing_usages
+
+				if showing_usages then
+					vim.cmd("LspLensOn")
+				else
+					vim.cmd("LspLensOff")
+				end
+
+				if showing_usages ~= require("symbol-usage").toggle_globally() then
+					require("symbol-usage").toggle()
+				end
+
+				if showing_usages then
+					vim.notify("Enabled Symbol Usage Inlay Hint", vim.log.levels.INFO)
+				else
+					vim.notify("Disabled Symbol Usage Inlay Hint", vim.log.levels.INFO)
+				end
+			end
+
+			vim.keymap.set("n", "<leader>tu", toggle_usages, { desc = "[T]oggle Symbol [U]sages" })
 		end,
 	},
 }
