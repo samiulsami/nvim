b5e7a38a27a6000347e4ff917414d3ce8d8fc24b
sami <sami@appscode.com>
2024-11-16 18:26:16 +0600

    Update project buffer script
    Signed-off-by: sami <sami@appscode.com>

--- lua/custom-scripts/project_buffers.lua
+++ lua/custom-scripts/project_buffers.lua
@@ -4,13 +4,34 @@ local project_buffers = function()
 	local buffers = vim.api.nvim_list_bufs()
 	local project_buffers = {}
 
+	local project_buffer_inclusion_patterns = {
+		project_root,
+	}
+
+	local project_buffer_exclusion_patterns = {
+		"neo-tree",
+		"Neotest",
+	}
+
 	for _, bufnr in ipairs(buffers) do
 		local filepath = vim.api.nvim_buf_get_name(bufnr)
-		if
-			filepath ~= ""
-			and filepath:find(project_root, 1, true)
-			and not filepath:find("neo-tree filesystem", 1, true)
-		then
+		local should_ignore = (filepath == "")
+
+		for _, include_pattern in ipairs(project_buffer_inclusion_patterns) do
+			if not filepath:find(include_pattern, 1, true) then
+				should_ignore = true
+				break
+			end
+		end
+
+		for _, ignore_pattern in ipairs(project_buffer_exclusion_patterns) do
+			if filepath:find(ignore_pattern, 1, true) then
+				should_ignore = true
+				break
+			end
+		end
+
+		if not should_ignore then
 			table.insert(project_buffers, {
 				bufnr = bufnr,
 				filename = filepath,
