0e83553320f9259066533dc533a0c67a1556ef3f
sami <sami@appscode.com>
2024-11-29 11:21:07 +0600

    Mimic Fish shell path view in statusline
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/lightline.lua
+++ lua/plugins/lightline.lua
@@ -9,13 +9,14 @@ return {
 			end
 			local components = vim.split(path, "/")
 			for i, component in ipairs(components) do
-				if i < #components then
-					local tmp = component:sub(1, 1)
-					if tmp == "." and #component > 1 then
-						components[i] = component:sub(1, 2)
-					else
-						components[i] = component:sub(1, 1)
-					end
+				if i >= #components - 1 then
+					break
+				end
+
+				if component:sub(1, 1) == "." and #component > 1 then
+					components[i] = component:sub(1, 2)
+				else
+					components[i] = component:sub(1, 1)
 				end
 			end
 			return table.concat(components, "/")
