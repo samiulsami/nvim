7b5871337fb054ae32c7ed6fbbb1ae20af48fe91
sami <sami@appscode.com>
2024-12-18 11:59:07 +0600

    Use snacks.words instead if manual lsp highlighting
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+			{ "williamboman/mason.nvim", config = true },
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{
@@ -30,53 +30,6 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("LSPDocumentHighlightAttachGroup32", { clear = true }),
-				callback = function(event)
-					local map = function(keys, func, desc, mode)
-						mode = mode or "n"
-						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup("LSPDocumentHighlightGroup32", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("LSPDocumentHighlightDetachGroup32", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({
-									group = "LSPDocumentHighlightGroup32",
-									buffer = event2.buf,
-								})
-							end,
-						})
-					end
-
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-						end, "[T]oggle Inlay [H]ints")
-					end
-				end,
-			})
-
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend(
 				"force",
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,33 +6,21 @@ return {
 
 		config = function()
 			local opts = {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
 				bigfile = {
-
 					enabled = true,
-					size = 50 * 1024 * 1024, -- 50 MB
-				},
-				dashboard = {
-					enabled = true,
-					sections = {
-						{ section = "header" },
-						{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-						{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-						{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-						{ section = "startup" },
-					},
+					size = 10 * 1024 * 1024, -- 10 MB
 				},
 				indent = {
 					enabled = true,
-					animate = {
-						enabled = false,
-					},
+					animate = { enabled = false },
 				},
 				input = { enabled = true },
 				quickfile = { enabled = true },
 				statuscolumn = { enabled = true },
+				words = {
+					enabled = true,
+					debounce = 50,
+				},
 			}
 
 			local Snacks = require("snacks")
@@ -41,10 +29,6 @@ return {
 			vim.keymap.set("n", "<leader>gp", function()
 				Snacks.gitbrowse()
 			end, { desc = "[G]ithub [P]review" })
-
-			vim.keymap.set("n", "<leader>db", function()
-				Snacks.dashboard()
-			end, { desc = "[D]ash[B]oard" })
 		end,
 	},
 }
