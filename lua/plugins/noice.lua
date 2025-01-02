9a88f9f6cb2af65faf6054bb445ebde374663886
sami <sami@appscode.com>
2025-01-02 19:01:14 +0600

    Disable annoying notifications
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
@@ -14,7 +11,7 @@ return {
 			{
 				"rcarriga/nvim-notify",
 				config = function()
-					vim.notify = require("notify") -- Set as the default notify handler
+					-- vim.notify = require("notify") -- Set as the default notify handler
 					require("notify").setup({
 						background_colour = "#101010",
 						stages = "fade",
@@ -27,12 +24,7 @@ return {
 		config = function()
 			require("noice").setup({
 				messages = {
-					enabled = true,
-					view = "false",
-					view_warn = "notify",
-					view_error = "notify",
-					view_search = "cmdline",
-					view_history = "messages",
+					enabled = false,
 				},
 
 				routes = {
@@ -40,15 +32,6 @@ return {
 						view = "cmdline",
 						filter = { event = "msg_showmode" },
 					},
-				},
-				notify = {
-					-- Noice can be used as `vim.notify` so you can route any notification like other messages
-					-- Notification messages have their level and other properties set.
-					-- event is always "notify" and kind can be any log level as a string
-					-- The default routes will forward notifications to nvim-notify
-					-- Benefit of using Noice for this is the routing and consistent history view
-					enabled = true,
-					view = "notify",
 				},
 				-- you can enable a preset for easier configuration
 				presets = {
