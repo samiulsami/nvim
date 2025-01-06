fae07c1d7fbd65350988b6f62285ed5d26a0fe6b
sami <sami@appscode.com>
2025-01-06 13:06:59 +0600

    fix formatting length
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -53,6 +53,59 @@ return {
 			local compare = require("cmp.config.compare")
 			luasnip.config.setup({})
 
+			-- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
+			formatting = {
+				fields = {
+					"abbr",
+					-- "menu",
+					"kind",
+				},
+				format = function(entry, item)
+					-- Define menu shorthand for different completion sources.
+					-- local menu_icon = {
+					-- 	nvim_lsp = "NLSP",
+					-- 	nvim_lua = "NLUA",
+					-- 	luasnip = "LSNP",
+					-- 	buffer = "BUFF",
+					-- 	path = "PATH",
+					-- }
+					-- -- Set the menu "icon" to the shorthand for each completion source.
+					-- item.menu = menu_icon[entry.source.name]
+
+					-- Set the fixed width of the completion menu to 60 characters.
+					local fixed_width = 60
+
+					-- Set 'fixed_width' to false if not provided.
+					fixed_width = fixed_width or false
+
+					-- Get the completion entry text shown in the completion window.
+					local content = item.abbr
+
+					-- Set the fixed completion window width.
+					if fixed_width then
+						vim.o.pumwidth = fixed_width
+					end
+
+					-- Get the width of the current window.
+					local win_width = vim.api.nvim_win_get_width(0)
+
+					-- Set the max content width based on either: 'fixed_width'
+					-- or a percentage of the window width, in this case 20%.
+					-- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+					local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+					-- Truncate the completion entry text if it's longer than the
+					-- max content width. We subtract 3 from the max content width
+					-- to account for the "..." that will be appended to it.
+					if #content > max_content_width then
+						item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+					else
+						item.abbr = content .. (" "):rep(max_content_width - #content)
+					end
+					return item
+				end,
+			}
+
 			local buffer_source = {
 				name = "buffer",
 				keyword_length = 2,
@@ -112,12 +165,7 @@ return {
 						compare.order,
 					},
 				},
-				formatting = {
-					format = require("lspkind").cmp_format({
-						with_text = true,
-						menu = { go_pkgs = "[pkgs]" },
-					}),
-				},
+				formatting = formatting,
 				window = {
 					completion = {
 						winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
@@ -142,6 +190,7 @@ return {
 					{ name = "nvim_lsp", keyword_length = 2 },
 					buffer_source,
 				}),
+				formatting = formatting,
 				matching = { disallow_symbol_nonprefix_matching = false },
 			})
 		end,
