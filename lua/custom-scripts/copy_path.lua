c9bb0716ddab52c0e69412d5132da23ae9da1887
sami <sami@appscode.com>
2025-02-24 17:35:30 +0600

    Simplify scripts
    Signed-off-by: sami <sami@appscode.com>

--- lua/custom-scripts/copy_path.lua
+++ lua/custom-scripts/copy_path.lua
@@ -1,49 +1,11 @@
---TODO: Automate this and integrate with LSP
-
-function Copy_project_relative_path()
-	local current_dir = vim.fn.expand("%:p:h")
-
-	local segments = {}
-	for segment in string.gmatch(current_dir, "[^/]+") do
-		table.insert(segments, segment)
-	end
-
-	local projectDirName = nil
-	local curPath = nil
-	local projectPathStartPos = 0
-
-	for _, s in pairs(segments) do
-		if curPath then
-			curPath = curPath .. "/" .. s
-		else
-			curPath = "/" .. s
-		end
-		if vim.fn.isdirectory(curPath .. "/.git") == 1 then
-			projectDirName = s
-			projectPathStartPos = #curPath - #s + 1
-			break
-		end
-	end
-
-	if projectDirName == nil or projectPathStartPos <= 0 or projectPathStartPos > #current_dir then
-		vim.notify("failed to identify project dir\ncurrent dir: " .. current_dir, vim.log.levels.ERROR)
-		return
-	end
-
-	local strippedPath = string.sub(current_dir, projectPathStartPos)
-	vim.fn.setreg("+", '"' .. strippedPath .. '"')
-	vim.notify("'" .. strippedPath .. "'\ncopied to clipboard", vim.log.levels.INFO)
-end
-
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>cp",
-	":lua Copy_project_relative_path()<CR>",
-	{ noremap = true, silent = true, desc = "[C]opy trimmed [P]ath" }
-)
-
-vim.keymap.set("n", "<leader>cP", function()
+vim.keymap.set("n", "<leader>cp", function()
 	local directory_path = vim.fn.expand("%:p:h")
 	vim.fn.setreg("+", '"' .. directory_path .. '"')
 	vim.notify("'" .. directory_path .. "'\ncopied to clipboard", vim.log.levels.INFO)
-end, { noremap = true, silent = true, desc = "[C]opy [P]ath" })
+end, { noremap = true, silent = true, desc = "[C]opy [P]ath to current file directory" })
+
+vim.keymap.set("n", "<leader>cP", function()
+	local current_file_path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", '"' .. current_file_path .. '"')
+	vim.notify("'" .. current_file_path .. "'\ncopied to clipboard", vim.log.levels.INFO)
+end, { noremap = true, silent = true, desc = "[C]opy [P]ath to current file" })
