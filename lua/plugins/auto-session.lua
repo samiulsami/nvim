bfb22ba32e2c2e89c39f455f5afcd8c288ed84cf
sami <sami@appscode.com>
2025-03-29 00:10:42 +0600

    show restore notification
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -7,6 +7,8 @@ return {
 	opts = {
 		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
 		cwd_change_handling = true,
+		continue_restore_on_error = false,
+		show_auto_restore_notif = true,
 	},
 	--stylua: ignore
 	keys = {
