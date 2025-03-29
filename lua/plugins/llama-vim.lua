5c5768ccb53290a35bd34889615d842b037d1d86
sami <sami@appscode.com>
2025-03-30 03:35:27 +0600

    llama.vim highlights and keybinds update
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/llama-vim.lua
+++ lua/plugins/llama-vim.lua
@@ -1,5 +1,6 @@
 return {
-	"ggml-org/llama.vim",
+	"samiulsami/llama.vim",
+	branch = "single-line",
 	init = function()
 		vim.g.llama_config = {
 			show_info = 0,
@@ -7,7 +8,12 @@ return {
 			auto_fim = true,
 			keymap_accept_full = "<C-j>",
 			keymap_accept_line = "<C-k>",
+			-- ring_n_chunks = 64,
+			-- ring_chunk_Size = 512,
+			-- ring_scope = 2048,
+			-- n_prefix = 2048,
 		}
+
 		vim.defer_fn(function()
 			vim.api.nvim_set_hl(0, "llama_hl_hint", { fg = "#859289", italic = true, ctermfg = 209 })
 		end, 0)
