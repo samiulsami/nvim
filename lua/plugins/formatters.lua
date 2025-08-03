4a0579b438e8809a6800dfbdac38656163170800
sami <sami@appscode.com>
2025-08-03 23:50:21 +0600

    Don't auto-format on save
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -1,35 +1,37 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
+	lazy = false,
 	cmd = { "ConformInfo" },
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			notify_on_error = false,
+			formatters_by_ft = {
+				lua = { "stylua" }, -- done
+				go = {
+					"gofumpt",
+					"goimports",
+					-- "golines",
+				},
+				cpp = { "clang-format" },
+				c = { "clang-format" },
+				yaml = { "yamlfix" },
+				json = { "fixjson" },
+				bash = { "shfmt" },
+				-- java = { "google-java-format" },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>F", function()
 			local disable_filetypes = { c = true, cpp = true }
 			local lsp_format_opt
+			local bufnr = vim.api.nvim_get_current_buf()
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				lsp_format_opt = "never"
 			else
 				lsp_format_opt = "fallback"
 			end
-			return {
-				timeout_ms = 1000,
-				lsp_format = lsp_format_opt,
-			}
-		end,
-		formatters_by_ft = {
-			lua = { "stylua" }, -- done
-			go = {
-				"gofumpt",
-				"goimports",
-				-- "golines",
-			},
-			cpp = { "clang-format" },
-			c = { "clang-format" },
-			yaml = { "yamlfix" },
-			json = { "fixjson" },
-			bash = { "shfmt" },
-			-- java = { "google-java-format" },
-		},
-	},
+			conform.format({ timeout_ms = 2000, lsp_format = lsp_format_opt })
+		end, { desc = "Format buffer" })
+	end,
 }
