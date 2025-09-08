1db0624764593783007dbf172600f770e27655fa
sami <sami@appscode.com>
2025-09-08 13:51:09 +0600

    Add vim-rhubarb
    Signed-off-by: sami <sami@appscode.com>

---
+++ lua/plugins/vim-rhubarb.lua
@@ -0,0 +1,15 @@
+return {
+	"tpope/vim-rhubarb",
+	dependencies = { "tpope/vim-fugitive" },
+	config = function()
+		vim.keymap.set({ "n" }, "<leader>B", "<cmd>GBrowse<CR>", { silent = true, noremap = true, desc = "Open the current line in Github" })
+		vim.keymap.set({ "v" }, "<leader>B", function()
+			local start_line = vim.fn.line("v")
+			local end_line = vim.fn.line(".")
+			if end_line < start_line then
+				start_line, end_line = end_line, start_line
+			end
+			vim.cmd(start_line .. "," .. end_line .. "GBrowse")
+		end, { silent = true, noremap = true, desc = "Open the current selection in Github" })
+	end,
+}
