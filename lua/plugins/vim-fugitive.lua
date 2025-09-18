ce59307d609be004d532d4a5e9a6db8f9f24e4cf
sami <sami@appscode.com>
2025-09-18 11:46:39 +0600

    Fix vim-fugitive commit keybind
    Signed-off-by: sami <sami@appscode.com>

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -9,7 +9,7 @@ return {
 			vim.keymap.set("n", "<leader>gl", "<Cmd>Git log --oneline --full-history<CR>", { desc = "[G]it [L]og Oneline" })
 			vim.keymap.set("n", "<leader>gA", "<Cmd>silent Git commit --amend --no-edit --signoff<CR>", { desc = "[G]it Commit [A]mend" })
 			vim.keymap.set("n", "<leader>gL", "<Cmd>Git log<CR>", { desc = "[G]it [L]og" })
-			vim.keymap.set("n", "<leader>gc", "<Cmd>Git commit --signoff<CR>", { desc = "[G]it [C]omit" })
+			vim.keymap.set("n", "<leader>gc", "<Cmd>silent Git commit --signoff<CR>", { desc = "[G]it [C]omit" })
 			-- stylua: ignore end
 		end,
 	},
