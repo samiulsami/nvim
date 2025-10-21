0ef36e077d56deb9eda719477084b70ec428f7c3
sami <samiulsami7786@gmail.com>
2025-10-21 14:16:57 +0600

    Fix mini.nvim loading issue
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- lua/plugins/mini.nvim.lua
+++ lua/plugins/mini.nvim.lua
@@ -16,5 +16,20 @@ return {
 				try_as_border = true,
 			},
 		})
+
+		require("mini.surround").setup({
+			mappings = {
+				add = "ys",
+				delete = "ds",
+				find = "",
+				find_left = "",
+				highlight = "",
+				replace = "cs",
+				suffix_last = "",
+				suffix_next = "",
+			},
+			search_method = "cover_or_next",
+		})
+		vim.api.nvim_del_keymap("v", "ys")
 	end,
 }
