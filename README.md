b5d059b43fef6dd527a986e23bab6f90ada1db38
sami <samiulsami7786@gmail.com>
2025-11-24 18:35:19 +0600

    only install mason packages if they're not installed in the system
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- README.md
+++ README.md
@@ -1,35 +1,20 @@
 # Neovim config based on kickstart.nvim
 
 #### Requirements
-```bash
-sudo apt update
-sudo apt install build-essential unzip git python3.12-venv xclip cmake gettext ripgrep
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-$HOME/.fzf/install
-
-#fd
-sudo apt install fd
-sudo ln -s $(which fdfind) /usr/bin/fd
-
-#rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup update
-
-#nodejs
-curl -fsSL https://fnm.vercel.app/install | zsh
-source $HOME/.zshrc
-fnm install 24
-```
+- ripgrep
+- xclip
+- fzf
+- cmake
+- rustup
+- unzip
 
 #### jdtls (optional)
 - setup java (21+)<br>
 - download [jdtls latest](https://www.eclipse.org/downloads/download.php?file=/jdtls/snapshots/jdt-language-server-latest.tar.gz)<br>
-- extract jdtls to `$HOME/.eclipse_jdtls`, make sure you have the `plugins`/`bin`/etc. dirs in the `$HOME/.eclipse_jdtls` folder<br>
+- extract jdtls to `$XDG_DATA_HOME/eclipse_jdtls`, make sure you have the `plugins`/`bin`/etc. dirs in the `$XDG_DATA_HOME/eclipse_jdtls` folder<br>
 - if needed, follow the steps here https://github.com/mfussenegger/nvim-jdtls<br>
 
-#### Build and configure
+#### Build from source and configure
 
 ```bash
 git clone https://github.com/neovim/neovim.git $HOME/neovim
@@ -50,4 +35,4 @@ git config --global core.editor 'nvim -f'
 ## Notes
 - `<leader>` key is space
 - Press `<leader>sk` to search keybinds
-- Most plugin keybinds are defined in their respective `.lua` file
+- Most plugin keybinds are defined in their respective `.lua` file in `./lua/plugins/*`
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,33 +5,54 @@ return {
 	},
 	lazy = false,
 	config = function()
+		local ensure_installed = {
+			"stylua",
+			"lua-language-server",
+			"bash-language-server",
+			"shellcheck",
+			"shfmt",
+			"checkmake",
+			"terraform",
+			"terraform-ls",
+			"tflint",
+			"jq",
+			"tree-sitter-cli",
+			"json-lsp",
+			"jsonlint",
+			"helm-ls",
+			"yaml-language-server",
+			"yamlfix",
+			"fixjson",
+			"gopls",
+			"golangci-lint",
+			"gofumpt",
+			"goimports",
+			"clangd",
+			"clang-format",
+			"cpplint",
+			"dockerfile-language-server",
+		}
+
+		-- for mason package names that don't match their executable names
+		local executable_mappings = {}
+		executable_mappings["tree-sitter-cli"] = "tree-sitter"
+
+		local to_install = {}
+		for _, app in ipairs(ensure_installed) do
+			local executable = executable_mappings[app] or app
+			local path = vim.fn.trim(vim.fn.system("which " .. executable))
+			if path == "" or vim.v.shell_error ~= 0 then
+				table.insert(to_install, app)
+			end
+		end
+
 		require("mason").setup()
 		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"stylua",
-				"lua-language-server",
-				"bash-language-server",
-				"shellcheck",
-				"shfmt",
-				"checkmake",
-				"terraform",
-				"terraform-ls",
-				"tflint",
-				"jq",
-				"tree-sitter-cli",
-				"json-lsp",
-				"jsonlint",
-				"helm-ls",
-				"yaml-language-server",
-				"yamlfix",
-				"fixjson",
-				"golangci-lint",
-				"gofumpt",
-				"goimports",
-				"clangd",
-				"clang-format",
-				"cpplint",
-				"dockerfile-language-server",
+			ensure_installed = to_install,
+			integrations = {
+				["mason-lspconfig"] = false,
+				["mason-null-ls"] = false,
+				["mason-nvim-dap"] = false,
 			},
 		})
 	end,
