46afca384ce333cd397cfcc643b3953e087b232d
sami <samiulsami7786@gmail.com>
2025-12-01 17:34:35 +0600

    Disable sidekick nes
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -4,6 +4,9 @@ return {
 		local sidekick = require("sidekick")
 		sidekick.setup({
 			cli = { watch = false },
+                        nes = {
+                                enabled = false
+                        }
 		})
 
 		local sidekick_cli = require("sidekick.cli")
