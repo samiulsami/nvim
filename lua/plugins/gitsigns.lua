1a680a9a28fb9adf0991ae378e154c7083bf58ac
sami <samiulsami7786@gmail.com>
2025-12-29 00:20:16 +0600

    reduce delay for showing git blame line
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -85,7 +85,7 @@ return {
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-					delay = 500,
+					delay = 100,
 					ignore_whitespace = false,
 					virt_text_priority = 0,
 					use_focus = true,
