bfca90daa02da38b580171cc789201cf831315cf
sami <samiulsami7786@gmail.com>
2026-01-08 14:07:25 +0600

    Use mini-pick for vim.ui_select
    Signed-off-by: sami <samiulsami7786@gmail.com>

---
+++ lua/plugins/mini-pick.lua
@@ -0,0 +1,34 @@
+return {
+	"echasnovski/mini.pick",
+	version = false,
+	config = function()
+		local mini_pick = require("mini.pick")
+		local win_config = function()
+			local height = math.floor(0.4 * vim.o.lines)
+			local width = math.floor(0.4 * vim.o.columns)
+			return {
+				anchor = "NW",
+				height = height,
+				width = width,
+				row = math.floor(0.5 * (vim.o.lines - height)),
+				col = math.floor(0.5 * (vim.o.columns - width)),
+			}
+		end
+
+		mini_pick.setup({
+			options = { content_from_bottom = false },
+			window = { config = win_config },
+			mappings = {
+				choose_all = {
+					char = "<C-q>",
+					func = function()
+						local matches = mini_pick.get_picker_matches()
+						if matches and matches.all and #matches.all > 0 then
+							mini_pick.default_choose_marked(matches.all)
+						end
+					end,
+				},
+			},
+		})
+	end,
+}
