059a4ae2c68d37ff41bbb98833dad1ebd28d45f4
sami <samiulsami7786@gmail.com>
2026-01-19 22:42:36 +0600

    Make luals aware of nvim runtime files
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,11 +1,10 @@
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-			},
+	"folke/lazydev.nvim",
+	ft = "lua",
+	lazy = false,
+	opts = {
+		library = {
+			{ path = "luvit-meta/library", words = { "vim%.uv" } },
 		},
 	},
 }
