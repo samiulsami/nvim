edf98f6b20451c66fad198baeef76dd6cdefb71a
sami <samiulsami7786@gmail.com>
2026-04-30 17:41:09 +0600

    Replace oil.nvim with canola.nvim
    Signed-off-by: sami <samiulsami7786@gmail.com>

---
+++ lua/plugins/canola.lua
@@ -0,0 +1,172 @@
+return {
+	"barrettruth/canola.nvim",
+	branch = "canola",
+
+	event = "VeryLazy",
+
+	---@module 'canola'
+	---@type canola.SetupOpts
+	opts = {},
+	init = function()
+		local columns = {
+			"size",
+			"mtime",
+			"permissions",
+		}
+
+		local fzf_lua = require("fzf-lua")
+		local fzf_lua_actions = require("fzf-lua.actions")
+
+		function _G.get_oil_winbar()
+			local dir = require("canola").get_current_dir()
+			if dir then
+				return vim.fn.fnamemodify(dir, ":~")
+			else
+				return vim.api.nvim_buf_get_name(0)
+			end
+		end
+
+		---@type canola.config
+		vim.g.canola = {
+			columns = {},
+
+			lsp = {
+				enabled = true,
+				autosave = true,
+			},
+
+			win = {
+				wrap = true,
+				winbar = "%!v:lua.get_oil_winbar()",
+			},
+
+			float = {
+				max_width = 0.999,
+				max_height = 0.999,
+				border = nil,
+				win = {
+					winblend = 4,
+				},
+			},
+
+			hidden = {
+				enabled = false,
+			},
+
+			keymaps = {
+				["<C-h>"] = {
+					callback = function()
+						local tmux_navigator = require("utils.tmux_navigation")
+						tmux_navigator.move_or_tmux(tmux_navigator.direction.LEFT)
+					end,
+					mode = "n",
+					desc = "Move to left window or tmux pane",
+				},
+				["<C-j>"] = {
+					callback = function()
+						local tmux_navigator = require("utils.tmux_navigation")
+						tmux_navigator.move_or_tmux(tmux_navigator.direction.DOWN)
+					end,
+					mode = "n",
+					desc = "Move to down window or tmux pane",
+				},
+				["<C-k>"] = {
+					callback = function()
+						local tmux_navigator = require("utils.tmux_navigation")
+						tmux_navigator.move_or_tmux(tmux_navigator.direction.UP)
+					end,
+					mode = "n",
+					desc = "Move to up window or tmux pane",
+				},
+				["<C-l>"] = {
+					callback = function()
+						local tmux_navigator = require("utils.tmux_navigation")
+						tmux_navigator.move_or_tmux(tmux_navigator.direction.RIGHT)
+					end,
+					mode = "n",
+					desc = "Move to right window or tmux pane",
+				},
+				["<leader>td"] = {
+					callback = function()
+						if #require("canola.config").columns ~= #columns then
+							require("canola").set_columns(columns)
+						else
+							require("canola").set_columns({})
+						end
+					end,
+					mode = "n",
+					desc = "[T]oggle Oil [D]etails",
+				},
+				["<leader>cp"] = {
+					callback = function()
+						local current_dir = require("canola").get_current_dir()
+						vim.fn.setreg("+", current_dir)
+						vim.notify("'" .. current_dir .. "'\ncopied to clipboard", vim.log.levels.INFO)
+					end,
+					mode = "n",
+					desc = "Copy Path",
+				},
+				["<leader>T"] = {
+					callback = function()
+						local dir = require("canola").get_current_dir()
+						vim.notify("Opening tmux split in directory: " .. dir, vim.log.levels.INFO)
+						local result = vim.fn.system({ "tmux", "split-window", "-c", dir })
+						if vim.v.shell_error ~= 0 then
+							vim.notify("Failed to open tmux split: " .. result, vim.log.levels.ERROR)
+							return
+						end
+					end,
+					mode = "n",
+					desc = "Open tmux split in current directory",
+				},
+				["<leader>sg"] = {
+					callback = function()
+						local canola = require("canola")
+						local dir = canola.get_current_dir()
+						vim.notify("Grep in current oil directory: " .. dir, vim.log.levels.INFO)
+						fzf_lua.live_grep({
+							cwd_prompt = true,
+							cwd = dir,
+							search = "",
+							actions = {
+								["default"] = function(selected, opts)
+									canola.close()
+									fzf_lua_actions.file_edit_or_qf(selected, opts)
+								end,
+							},
+						})
+					end,
+					mode = "n",
+					nowait = false,
+					desc = "Grep in current oil directory",
+				},
+				["<leader>sf"] = {
+					callback = function()
+						local canola = require("canola")
+						local dir = canola.get_current_dir()
+						fzf_lua.files({
+							cwd_prompt = true,
+							cwd = dir,
+							actions = {
+								["default"] = function(selected, opts)
+									canola.close()
+									fzf_lua_actions.file_edit_or_qf(selected, opts)
+								end,
+							},
+						})
+					end,
+					mode = "n",
+					nowait = false,
+					desc = "Search in Current oil directory",
+				},
+			},
+
+			watch = true,
+			cursor = true,
+		}
+	end,
+
+	config = function()
+		vim.keymap.set("n", "<leader>p", require("canola").toggle_float, { desc = "Toggle Canola" })
+	end,
+}
