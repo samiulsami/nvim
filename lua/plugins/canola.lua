79d74027974768bb8e0105cfbbe4c276f594f7f9
sami <samiulsami7786@gmail.com>
2026-04-30 18:39:01 +0600

    Simplify canola.nvim configuration
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- lua/plugins/canola.lua
+++ lua/plugins/canola.lua
@@ -17,15 +17,6 @@ return {
 		local fzf_lua = require("fzf-lua")
 		local fzf_lua_actions = require("fzf-lua.actions")
 
-		function _G.get_oil_winbar()
-			local dir = require("canola").get_current_dir()
-			if dir then
-				return vim.fn.fnamemodify(dir, ":~")
-			else
-				return vim.api.nvim_buf_get_name(0)
-			end
-		end
-
 		---@type canola.config
 		vim.g.canola = {
 			columns = {},
@@ -37,7 +28,6 @@ return {
 
 			win = {
 				wrap = true,
-				winbar = "%!v:lua.get_oil_winbar()",
 			},
 
 			float = {
@@ -95,7 +85,7 @@ return {
 						end
 					end,
 					mode = "n",
-					desc = "[T]oggle Oil [D]etails",
+					desc = "[T]oggle Canola [D]etails",
 				},
 				["<leader>cp"] = {
 					callback = function()
@@ -108,9 +98,8 @@ return {
 				},
 				["<leader>T"] = {
 					callback = function()
-						local dir = require("canola").get_current_dir()
-						vim.notify("Opening tmux split in directory: " .. dir, vim.log.levels.INFO)
-						local result = vim.fn.system({ "tmux", "split-window", "-c", dir })
+						local result =
+							vim.fn.system({ "tmux", "split-window", "-c", require("canola").get_current_dir() })
 						if vim.v.shell_error ~= 0 then
 							vim.notify("Failed to open tmux split: " .. result, vim.log.levels.ERROR)
 							return
@@ -122,11 +111,9 @@ return {
 				["<leader>sg"] = {
 					callback = function()
 						local canola = require("canola")
-						local dir = canola.get_current_dir()
-						vim.notify("Grep in current oil directory: " .. dir, vim.log.levels.INFO)
 						fzf_lua.live_grep({
 							cwd_prompt = true,
-							cwd = dir,
+							cwd = canola.get_current_dir(),
 							search = "",
 							actions = {
 								["default"] = function(selected, opts)
@@ -138,15 +125,14 @@ return {
 					end,
 					mode = "n",
 					nowait = false,
-					desc = "Grep in current oil directory",
+					desc = "Grep in current Canola directory",
 				},
 				["<leader>sf"] = {
 					callback = function()
 						local canola = require("canola")
-						local dir = canola.get_current_dir()
 						fzf_lua.files({
 							cwd_prompt = true,
-							cwd = dir,
+							cwd = canola.get_current_dir(),
 							actions = {
 								["default"] = function(selected, opts)
 									canola.close()
@@ -157,7 +143,7 @@ return {
 					end,
 					mode = "n",
 					nowait = false,
-					desc = "Search in Current oil directory",
+					desc = "Search in Current Canola directory",
 				},
 			},
 
