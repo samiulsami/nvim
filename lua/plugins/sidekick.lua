af249fe554883835160529bbc693f96d7f655d79
sami <samiulsami7786@gmail.com>
2026-05-07 18:12:30 +0600

    Add back sidekick.nvim
    Signed-off-by: sami <samiulsami7786@gmail.com>

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,39 @@
+return {
+	"folke/sidekick.nvim",
+	config = function()
+		local sidekick = require("sidekick")
+		sidekick.setup({
+			cli = { watch = false },
+			nes = { enabled = false },
+		})
+
+		local sidekick_cli = require("sidekick.cli")
+		vim.keymap.set({ "n", "v" }, "<A-c>", function()
+			local full_path = vim.fn.expand("%:p")
+
+			local start_line = vim.fn.line("v")
+			local end_line = vim.fn.line(".")
+
+			if start_line > end_line then
+				start_line, end_line = end_line, start_line
+			end
+
+			local line_ref = start_line == end_line and string.format("#%d ", start_line)
+				or string.format("[offset=%d, limit=%d]", start_line, end_line - start_line)
+
+			sidekick_cli.send({
+				name = "opencode",
+				msg = string.format("\n@%s%s", full_path, line_ref),
+				focus = false,
+			})
+		end, { desc = "Sidekick send to cli" })
+
+		local sidekick_nes = require("sidekick.nes")
+
+		vim.keymap.set({ "n", "i" }, "<A-;>", function()
+			if sidekick_nes.have() then
+				sidekick_nes.apply()
+			end
+		end, { desc = "Show Sidekick NES" })
+	end,
+}
