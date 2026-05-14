4cb0b63548b0fba629d7f47f371f9d7b9133690b
sami <samiulsami7786@gmail.com>
2026-05-14 23:16:30 +0600

    Add nvim-tree
    Signed-off-by: sami <samiulsami7786@gmail.com>

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,42 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		---@type nvim_tree.config
+		local config = {
+			hijack_cursor = true,
+			hijack_directories = {
+				enable = true,
+				auto_open = false,
+			},
+			update_focused_file = {
+				enable = true,
+				update_root = {
+					enable = true,
+				},
+			},
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 30,
+				number = true,
+				relativenumber = true,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		}
+		require("nvim-tree").setup(config)
+
+		vim.keymap.set("n", "<A-p>", function()
+			require("nvim-tree.api").tree.toggle({
+				find_file = true,
+				update_root = true,
+				focus = true,
+			})
+		end, { desc = "Toggle nvim-tree" })
+	end,
+}
