7c80c99cf7716108d1b5ad7c3106f95adcafea81
sami <samiulsami7786@gmail.com>
2026-06-18 13:34:42 +0600

    Replace dart.nvim with quickbuf.nvim
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- lua/plugins/quickbuf.lua
+++ lua/plugins/quickbuf.lua
@@ -2,10 +2,24 @@
 return {
 	src = "https://github.com/tjgao/quickbuf.nvim",
 	config = function()
-		require("quickbuf").setup()
-		vim.keymap.set("n", "<Tab>", "<cmd>QuickBuf<CR>", { desc = "QuickBuf" })
-		vim.keymap.set("n", "<leader>qt", "<cmd>QuickBufPinToggle<CR>", { desc = "Pin toggle" })
-		vim.keymap.set("n", "<S-h>", "<cmd>QuickBufPrevPinned<CR>", { desc = "Prev pinned buffer" })
-		vim.keymap.set("n", "<S-l>", "<cmd>QuickBufNextPinned<CR>", { desc = "Next pinned buffer" })
+		require("quickbuf").setup({
+			include_special = false,
+			alternate_key = "<A-s>",
+			alternate_key_display = "<A-s>",
+			alternate_without_label = false,
+			show_icons = false,
+			picker = {
+				move_up_key = "<C-p>",
+				move_down_key = "<C-n>",
+				toggle_pin_key = "p",
+			},
+			persistence = {
+				enabled = true,
+			},
+		})
+		vim.keymap.set("n", "<A-s>", "<cmd>QuickBuf<CR>", { desc = "QuickBuf" })
+		vim.keymap.set("n", "<A-w>", "<cmd>QuickBufPinToggle<CR>", { desc = "Pin toggle" })
+		vim.keymap.set("n", "<A-q>", "<cmd>QuickBufPrevPinned<CR>", { desc = "Prev pinned buffer" })
+		vim.keymap.set("n", "<A-e>", "<cmd>QuickBufNextPinned<CR>", { desc = "Next pinned buffer" })
 	end,
 }
