0bc7075fdddf2f0c743b6a0a459a1617d0371f72
sami <samiulsami7786@gmail.com>
2026-08-24 13:00:59 +0600

    Add leetcode.nvim
    Signed-off-by: sami <samiulsami7786@gmail.com>

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,26 @@
+---@type PluginSpec
+return {
+	src = "https://github.com/kawre/leetcode.nvim",
+	deps = {
+		{ src = "https://github.com/nvim-lua/plenary.nvim" },
+		{ src = "https://github.com/MunifTanjim/nui.nvim" },
+	},
+	config = function()
+		require("leetcode").setup({
+			lang = "golang",
+			plugins = {
+				non_standalone = true,
+			},
+			description = {
+				position = "left",
+				width = "40%",
+				show_stats = true,
+			},
+			picker = {
+				provider = "fzf-lua",
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cl", "<Cmd>Leet<CR>", { desc = "Open LeetCode" })
+	end,
+}
