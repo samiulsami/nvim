7be882fd2ecea8ef46aba56db55f64e1f0f84701
sami <samiulsami7786@gmail.com>
2026-09-02 14:03:13 +0600

    Add parts of speech highlighter (hi-pos.nvim)
    Signed-off-by: sami <samiulsami7786@gmail.com>

---
+++ lua/plugins/hi-pos.lua
@@ -0,0 +1,9 @@
+---@type PluginSpec
+return {
+	src = "https://github.com/maxonvim/hi-pos.nvim",
+	config = function()
+		local pos = require("hi_pos").setup()
+
+		vim.keymap.set("n", "<leader>h", pos.start, { desc = "Start POS highlighting" })
+	end,
+}
